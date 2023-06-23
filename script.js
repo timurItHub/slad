d41e49cf4526b97f87dc6f36240e95a5b9051508
timur <timurageevv@gmail.com>
2023-06-24 01:59:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const slider = document.getElementById("mySlider");
+const output = document.getElementById("sliderValue");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
